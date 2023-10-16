cf7d4d995dfb096bc19cb5515b5f7ed08c5c5f97
Maximilian Kraft <kraft@spengergasse.at>
2023-10-16 12:02:48 +0200

    initial commit

---
+++ src/main/java/at/spengergasse/mgin/apis/ApisApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.mgin.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisApplication.class, args);
+	}
+
+}
