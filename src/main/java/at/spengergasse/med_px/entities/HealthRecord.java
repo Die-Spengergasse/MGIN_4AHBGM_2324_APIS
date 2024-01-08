c6e7d9323f9b353d3e0b78e71aaecd1771a5e0f8
luka0911 <rad22643@spengergasse.at>
2024-01-08 17:07:19 +0100

    3 Klassen eingefügt; Entity - Klasse angepasst

--- src/main/java/at/spengergasse/med_px/entities/HealthRecord.java
+++ src/main/java/at/spengergasse/med_px/entities/HealthRecord.java
@@ -9,12 +9,82 @@ import java.util.List;
  */
 @Entity
 public class HealthRecord {
+
     @Id
     @GeneratedValue
     private Long id;
+
     @OneToOne
     @JoinColumn(name = "patient_id")
     private Patient patient;        // the patient who owns the health record
+
     @ManyToMany
     private List<Allergy> allergies;    // the allergies of the patient
+
+    private String bloodgroup;
+
+    public String medication;
+
+    public HealthRecord() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public void setAllergies(List<Allergy> allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getBloodgroup() {
+        return bloodgroup;
+    }
+
+    public void setBloodgroup(String bloodgroup) {
+        this.bloodgroup = bloodgroup;
+    }
+
+    public HealthRecord(Long id, Patient patient, List<Allergy> allergies, String bloodgroup, String medication){
+        this.id = id;
+        this.patient = patient;
+        this.allergies = allergies;
+        this.bloodgroup = bloodgroup;
+        this.medication = medication;
+    }
+
+    @Override
+    public String toString() {
+        return "HealthRecord{" +
+                "id=" + id +
+                ", patient=" + patient +
+                ", allergies=" + allergies +
+                ", bloodgroup='" + bloodgroup + '\'' +
+                ", medication='" + medication + '\'' +
+                '}';
+    }
+
+    public List<Allergy> getAllergies() {
+        return allergies;
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
+    public void setMedication(String medication) {
+        this.medication = medication;
+    }
 }
---
+++ src/main/java/at/spengergasse/med_px/healthRecord/HealthRecordMvcConfig.java
@@ -0,0 +1,4 @@
+package at.spengergasse.med_px.healthRecord;
+
+public class HealthRecordMvcConfig {
+}
