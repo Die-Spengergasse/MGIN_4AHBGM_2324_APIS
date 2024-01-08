8350b25ec7a7cd785eb9bc0a4a43f98027623fdf
JannatFarooqi <FAR22183@spengergasse.at>
2024-01-08 17:07:24 +0100

    Controller, Repository, MvcConfig und .js Dateien hinzugefügt. Außerdem Entitiy Klasse "Contact" angepasst

--- src/main/java/at/spengergasse/med_px/entities/Contact.java
+++ src/main/java/at/spengergasse/med_px/entities/Contact.java
@@ -7,9 +7,10 @@ import jakarta.persistence.*;
  * a patient may have more than one contact
  */
 @Entity
+@Table(name = "contact")
 public class Contact {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
@@ -19,4 +20,75 @@ public class Contact {
     @ManyToOne
     @JoinColumn(name = "city_zip")
     private City city;              // the city of the contact
+
+    @ManyToOne
+    @JoinColumn(name = "email")
+    private Patient email;
+
+    @ManyToOne
+    @JoinColumn(name = "phone")
+    private Patient phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Patient getEmail() {
+        return email;
+    }
+
+    public void setEmail(Patient email) {
+        this.email = email;
+    }
+
+    public Patient getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Patient phone) {
+        this.phone = phone;
+    }
+
+    public Contact(Long id, Patient patient, City city, Patient email, Patient phone) {
+        this.id = id;
+        this.patient = patient;
+        this.city = city;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Contact(){
+
+    };
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", patient=" + patient +
+                ", city=" + city +
+                ", email=" + email +
+                ", phone=" + phone +
+                '}';
+    }
 }
---
+++ src/main/java/at/spengergasse/med_px/kontaktdaten/ContactRepository.java
@@ -0,0 +1,4 @@
+package at.spengergasse.med_px.kontaktdaten;
+
+public interface ContactRepository {
+}
