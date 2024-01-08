52347ef5a8e2242b1a484b59c0d7e7325cb3454f
simon <simonkotvas9@icloud.com>
2024-01-08 17:06:44 +0100

    commit1

---
+++ src/main/resources/static/js1.js
@@ -0,0 +1,9 @@
+
+
+
+<button onclick="switchToPage2()">Go to Page 2</button>
+
+
+    function switchToPage2() {
+    window.location.href = "page2.html";
+}
---
+++ src/main/resources/static/js2.js
@@ -0,0 +1,6 @@
+<button onclick="switchToPage1()">Go to Page 1</button>
+
+
+    function switchToPage1() {
+    window.location.href = "index.html";
+    }
