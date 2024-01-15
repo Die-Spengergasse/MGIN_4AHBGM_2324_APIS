0b1efea46bb03a576be2e1add48587558e38affe
Teodora Petrovska <pet22651@spengergasse.at>
2024-01-15 15:28:43 +0100

    haha-

---
+++ src/main/java/konsultation/ConsulattaionContr.java
@@ -0,0 +1,76 @@
+package at.spengergasse.konsultation.controller;
+
+import at.spengergasse.konsultation.entities.Konsultation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/konsultation")
+public class ConsulattaionContr {
+    private ArrayList<Konsultation> konsultationList = new ArrayList<>();
+
+
+    public ConsulattaionContr() {
+        konsultationList.add(new Konsultation("Jannat", LocalDate.of(2023, 01, 26), "Fieber", "Erkaeltung", "Vitamine", 3));
+        konsultationList.add(new Konsultation("Labi", LocalDate.of(2023, 10, 06), "Husten", "Erkaeltung", "Antibiotika", 2));
+        konsultationList.add(new Konsultation("Riham", LocalDate.of(2023, 07, 14), "Kopfweh", "Krank", "Paracetamol", 1));
+        konsultationList.add(new Konsultation("Aleksandra", LocalDate.of(2023, 04, 11), "Schnupfen", "Krank", "Vitamine", 4));
+    }
+
+    @GetMapping("/all")
+    public ArrayList<Konsultation> getAllKonsultationen() {
+        return konsultationList;
+    }
+
+    @GetMapping("/{name}")
+    public Konsultation getKonsultationByName(@PathVariable String name) {
+        return konsultationList.stream()
+                .filter(konsultation -> konsultation.getName() == name)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @GetMapping("/patients")
+    public List<Konsultation> getKonsultationByPatientName(@RequestParam("name") String name) {
+        return konsultationList.stream()
+                .filter(konsultation -> konsultation.getName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/add")
+    public void addKonsultation(@RequestBody Konsultation newKonsultation) {
+        konsultationList.add(newKonsultation);
+    }
+
+    @PutMapping("/patients/{name}")
+    public Konsultation updateKonsultationByName(@PathVariable String name, @RequestBody Konsultation updatedKonsultation) {
+        for (Konsultation konsultation : konsultationList) {
+            if (konsultation.getName() == name) {
+                konsultation.setAbsencedates(updatedKonsultation.getAbsencedates());
+                konsultation.setDate(updatedKonsultation.getDate());
+                konsultation.setSymptoms(updatedKonsultation.getSymptoms());
+                konsultation.setDiagnosis(updatedKonsultation.getDiagnosis());
+                konsultation.setPrescription(updatedKonsultation.getPrescription());
+                return konsultation;
+            }
+        }
+        return null;
+    }
+    @DeleteMapping("/{name}")
+    public void deleteKonsultationByName(@PathVariable String name) {
+        Iterator<Konsultation> iterator = konsultationList.iterator();
+        while (iterator.hasNext()) {
+            Konsultation konsultation = iterator.next();
+            if (konsultation.getName() == name) {
+                iterator.remove();
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/konsultation/Konsultation.java
@@ -0,0 +1,75 @@
+package at.spengergasse.konsultation.entities;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.time.LocalDate;
+
+
+public class Konsultation {
+
+    private String name;
+    private LocalDate date;
+    private String symptoms;
+    private String diagnosis;
+    private String prescription;
+    private int absencedates;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(String prescription) {
+        this.prescription = prescription;
+    }
+
+    public int getAbsencedates() {
+        return absencedates;
+    }
+
+    public void setAbsencedates(int absencedates) {
+        this.absencedates = absencedates;
+    }
+
+    public Konsultation(String name, LocalDate date, String symptoms, String diagnosis, String prescription, int absencedates) {
+        this.name = name;
+        this.date = date;
+        this.symptoms = symptoms;
+        this.diagnosis = diagnosis;
+        this.prescription = prescription;
+        this.absencedates = absencedates;
+    }
+
+    public Konsultation() {
+    }
+}
