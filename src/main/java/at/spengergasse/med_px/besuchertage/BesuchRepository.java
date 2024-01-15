c7b5212089f0b53b43d6fa5eecc1c979e66c4996
SINO420 <sino.akrout@gmail.com>
2024-01-15 16:54:35 +0100

    Revert "projekt hinzugefügt und an git angebunde"
    This reverts commit 272b910753c501c967a0490d321a15eb3f474987.

--- src/main/java/at/spengergasse/med_px/besuchertage/BesuchRepository.java
+++
@@ -1,9 +0,0 @@
-package at.spengergasse.med_px.besuchertage;
-
-import at.spengergasse.med_px.entities.Patientenbesuch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BesuchRepository extends JpaRepository<Patientenbesuch, Integer> {
-}
