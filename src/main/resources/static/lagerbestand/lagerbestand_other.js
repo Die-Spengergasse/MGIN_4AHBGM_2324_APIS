7a106d6180f61f3dbe82902762cf394b92ca832c
ramazan888 <ramazan.atalay888@gmail.com>
2024-01-22 16:53:55 +0100

    stand 22.1.24

--- src/main/resources/static/lagerbestand/lagerbestand_other.js
+++ src/main/resources/static/lagerbestand/lagerbestand_other.js
@@ -1,15 +1,29 @@
-async function load_lagerbestand2(){
-    const result = await fetch("/api/lagerbestand/lagerbestand/all");
-    const lagerbestand = await result.json();
-    for(const l of lagerbestand){
-        const tbody = document.querySelector("#lagerbestand_list");
-        const row = tbody.insertRow();
-        row.innerHTML = `
-                <td>${l.id}</td>
-                <td>${l.name}</td>
-                <td>${l.quantity}</td>
-                <td>${l.fachnr}</td>
-        `;
+const lagerbestaende = [];
 
+
+async function load() {
+    // get book list from REST service
+    const response  =  await fetch("/api/lagerbestand/lagerbestand/all");
+    const lagerbestaende =  await response.json();
+
+    const body = document.querySelector("#Tabelle-tabelle");
+    for (const l of lagerbestaende) {
+        body.insertRow().innerHTML = `<td>${l.verbrauchsmaterial}</td>
+            <td>${l.quantity}</td><td>${l.fachnr}</td>`;
+    }
+
+    const lagerbestand1 = lagerbestaende[0];
+
+    const init = {
+        method : "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body : JSON.stringify(lagerbestand1)
+    };
+    const rc = await fetch("/api/lagerbestand/", init);
+    console.log(rc);
+    if (rc.status != 200){
+        alert("error sending verbrauchsmaterial to server");
     }
 }
