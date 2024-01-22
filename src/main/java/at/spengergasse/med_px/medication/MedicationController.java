fe94a9e1780a3cbb3ce772defd650e1e10f5cdcf
betyl7 <betylkose@gmail.com>
2024-01-22 20:03:29 +0100

    server port in application.properties eingefügt MedicationController gemacht MedicationMvcConfig weitergemacht / korrigiert In medication.html und masterpage.html gearbeitet - Befehle eingefügt

--- src/main/java/at/spengergasse/med_px/medication/MedicationController.java
+++ src/main/java/at/spengergasse/med_px/medication/MedicationController.java
@@ -1,4 +1,52 @@
 package at.spengergasse.med_px.medication;
 
+import at.spengergasse.med_px.entities.Medication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/medikation")
 public class MedicationController {
+
+    private ArrayList<Medication> medikations = new ArrayList<>();
+
+    @GetMapping("/all")
+    public List<Medication> allMedikations() {
+        medikations.add(new Medication(1L,"Nurofen", "200mg", "Mittags", 13, "alles bestens"));
+        medikations.add(new Medication(2L,"Parkemed", "250mg", "Abends", 13, "starke Kopfschmerzen"));
+        return medikations;}
+
+    @GetMapping("/{id}")
+    public Medication getMedicationsById(@PathVariable int id) {
+        return medikations.stream().filter(medikation -> medikation.getId() == id).findFirst().orElse(null);
+    }
+
+    @GetMapping
+    public List<Medication> getMedikationsByName (@RequestParam String medikamentenname) {
+        return medikations.stream()
+                .filter(medikation -> medikation.getMedikamentenname().equals(medikamentenname)).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public Medication addMedikations(@RequestBody Medication medikation) {
+        medikations.add(medikation);
+        return medikation;
+    }
+
+    @PutMapping("/{id}")
+    public Medication updatePatient(@PathVariable int id, @RequestBody Medication updateMedikation) {
+        medikations = (ArrayList<Medication>) medikations.stream()
+                .map(medikation -> (medikation.getId() == id) ?updateMedikation : medikation)
+                .collect(Collectors.toList());
+        return updateMedikation;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMedikation(@PathVariable int id) {
+        medikations.removeIf(medikation -> medikation.getId() == id);
+    }
 }
--- src/main/java/at/spengergasse/med_px/medication/MedicationRepository.java
+++ src/main/java/at/spengergasse/med_px/medication/MedicationRepository.java
@@ -1,4 +1,8 @@
 package at.spengergasse.med_px.medication;
 
-public class MedicationRepository {
+import at.spengergasse.med_px.entities.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+
 }
